e0755893a6882fd9bc87b10df73ca260ec033a72
shalapy <salahshalapy999@gmail.com>
2025-01-11 00:11:31 +0200

    Cleaned ContextAPI Code

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -7,53 +7,6 @@ const CartContext = createContext({
   ClearCart: () => {},
 });
 
-// function cartReducer(state, action) {
-//   if (action.type === "ADD_ITEM") {
-//     const existingCartItemIndex = state.items.findIndex(
-//       (item) => item.id === action.item.id
-//     );
-
-//     const updatedItems = [...state.items];
-
-//     if (existingCartItemIndex > -1) {
-//       const existingItem = state.items[existingCartItemIndex];
-//       const updatedItem = {
-//         ...existingItem,
-//         quantity: existingItem.quantity + 1,
-//       };
-//       updatedItems[existingCartItemIndex] = updatedItem;
-//     } else {
-//       updatedItems.push({ ...action.item, quantity: 1 });
-//     }
-
-//     return { ...state, items: updatedItems };
-//   }
-
-//   if (action.type === "REMOVE_ITEM") {
-//     const existingCartItemIndex = state.items.findIndex(
-//       (item) => item.id === action.id
-//     );
-
-//     const existingaCartItem = state.items[existingCartItemIndex];
-//     const updatedItems = [...state.items];
-
-//     if (existingaCartItem) {
-//       updatedItems.splice(existingCartItemIndex, 1);
-//     } else {
-//       const updatedItem = {
-//         ...existingaCartItem,
-//         quantity: existingaCartItem.quantity - 1,
-//       };
-
-//       updatedItems[existingCartItemIndex] = updatedItem;
-//     }
-
-//     return { ...state, item: updatedItems };
-//   }
-
-//   return state;
-// }
-
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
